bf18cf508069734df9b6562b52289cdec996bf73
Nikesh Parajuli <nikesh@rachanas-macbook-pro.local>
2018-11-03 14:08:43 -0600

    Optional steps - Lab 1

--- FlashCards/ViewController.swift
+++ FlashCards/ViewController.swift
@@ -12,15 +12,64 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var backLabel: UILabel!
     @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var card: UIView!
+    
+    @IBOutlet weak var btnOptionOne: UIButton!
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    @IBOutlet weak var btnOptionThree: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        card.layer.cornerRadius = 20.0
+        frontLabel.layer.cornerRadius = 20.0
+        backLabel.layer.cornerRadius = 20.0
+        
+        btnOptionOne.layer.cornerRadius = 20.0
+        btnOptionOne.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        btnOptionOne.layer.borderWidth = 3.0
+        
+        btnOptionTwo.layer.cornerRadius = 20.0
+        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        btnOptionTwo.layer.borderWidth = 3.0
+        
+        btnOptionThree.layer.cornerRadius = 20.0
+        btnOptionThree.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        btnOptionThree.layer.borderWidth = 3.0
+      
+        card.layer.shadowRadius = 15.0
+        frontLabel.layer.shadowRadius = 15.0
+        backLabel.layer.shadowRadius = 15.0
+        
+        card.layer.shadowOpacity = 0.2
+        frontLabel.layer.shadowOpacity = 0.2
+        backLabel.layer.shadowOpacity = 0.2
+        
+        frontLabel.clipsToBounds = true
+        backLabel.clipsToBounds = true
     }
 
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        frontLabel.isHidden = true;
+        if frontLabel.isHidden == true{
+            frontLabel.isHidden = false;
+        }
+        else{
+            frontLabel.isHidden = true;
+        }
     }
+    
+    @IBAction func didTapOptionOne(_ sender: Any) {
+        btnOptionOne.isHidden = true
+    }
+    
+    @IBAction func didTapOptionTwo(_ sender: Any) {
+        frontLabel.isHidden = true
+    }
+    
+    @IBAction func didTapOptionThree(_ sender: Any) {
+        btnOptionThree.isHidden = true
+    }
+    
 }
 
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ## NAME_OF_YOUR_APP_HERE
 
 ### App Description
-A flashcard app that displays an answer to the question when tapped on the question. 
+A flashcard app that displays an answer to the question when tapped on the question, has multiple choice options and displays the answer when clicked on the correct option.
 
 ### App Walk-though
 
-<img src="http://g.recordit.co/aPW42G81Vs.gif" width=200><br>
+<img src="http://g.recordit.co/7BMCJkGOZf.gif" width=200><br>
 
 ## Required
 - [x] Create New Project in Xcode
@@ -14,6 +14,6 @@ A flashcard app that displays an answer to the question when tapped on the quest
 - [x] Build in logic to show the answer side when the card is tapped
 - [x] Push code to GitHub
 ## Optional
-- [ ] Toggle the flashcard between the question side and the answer side
-- [ ] Style the question and answer side of the card to better distinguish between the two sides
-- [ ] Add selectable multiple choice answers beneath the card
+- [x] Toggle the flashcard between the question side and the answer side
+- [x] Style the question and answer side of the card to better distinguish between the two sides
+- [x] Add selectable multiple choice answers beneath the card
